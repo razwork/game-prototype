c101a8016960af55a12a3c856fa20dd2f90dfa75
Raz Rashid <r.rashid376@gmail.com>
2019-12-06 09:42:11 +0000

    Updates

---
+++ precache-manifest.185f260a0951d8ad68c039b73c5ba11a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490f86ea59a214dde13449ae02d710b5",
+    "url": "/cc-g-proto/index.html"
+  },
+  {
+    "revision": "66efa5cdfe81c0be35c7",
+    "url": "/cc-g-proto/static/css/main.8ef9dc45.chunk.css"
+  },
+  {
+    "revision": "f5f723cf870d14b0a1fe",
+    "url": "/cc-g-proto/static/js/2.f32a772e.chunk.js"
+  },
+  {
+    "revision": "66efa5cdfe81c0be35c7",
+    "url": "/cc-g-proto/static/js/main.2e2541db.chunk.js"
+  },
+  {
+    "revision": "13fc01bcf685bd932384",
+    "url": "/cc-g-proto/static/js/runtime-main.9f639264.js"
+  }
+]);
